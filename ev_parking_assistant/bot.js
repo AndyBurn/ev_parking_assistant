1c31f41a4680a4a985b07689c2202aed119995b2
Alex Gachanja <alex.a.gachanja@gmail.com>
2022-05-30 20:24:51 +0300

    Reset commits

--- ev_parking_assistant/bot.js
+++ ev_parking_assistant/bot.js
@@ -3,13 +3,33 @@
 
 const { ActivityHandler, MessageFactory } = require('botbuilder');
 
+const { QnAMaker } = require('botbuilder-ai');
+
 class EchoBot extends ActivityHandler {
-    constructor() {
+    constructor(configuration, qnaOptions) {
         super();
+        if(!configuration) throw new Error('[QnaMakerBot]: Missing parameter. Configuration is required');
+        // Create a QnAMaker connector
+        this.qnaMaker = new QnAMaker(configuration.QnAConfiguration, qnaOptions);
+
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
+            // Send user input to QnA Maker
+            const qnaResults = await this.qnaMaker.getAnswers(context);
+            // If an answer was received from QnA Maker, send the answer back to the user.
+            if(qnaResults[0]){
+                console.log(qnaResults[0])
+                await context.sendActivity(`${qnaResults[0].answer}`);
+            }
+            else{
+                // If no answers were returned from QnA Amker, reply with help.
+                await context.sendActivity(`I'm not sure`
+                        + 'I found an answer to your question'
+                        + `You can ask me questions about electric vehicles like 
+                        "how can I charge my car?"`);
+            }
+            // const replyText = `Echo: ${ context.activity.text }`;
+            // await context.sendActivity(MessageFactory.text(replyText, replyText));
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
--- ev_parking_assistant/index.js
+++ ev_parking_assistant/index.js
@@ -68,8 +68,20 @@ const onTurnErrorHandler = async (context, error) => {
 // Set the onTurnError for the singleton CloudAdapter.
 adapter.onTurnError = onTurnErrorHandler;
 
+// Map configuration values from .env file into the required format for each service.
+const QnAConfiguration = {
+    knowledgeBaseId: process.env.QnAKnowledgebaseId,
+    endpointKey: process.env.QnAAuthKey,
+    host: process.env.QnAEndpointHostName
+};
+
+//pack each service configuration into
+const configuration = {
+    QnAConfiguration
+}
+
 // Create the main dialog.
-const myBot = new EchoBot();
+const myBot = new EchoBot(configuration);
 
 // Listen for incoming requests.
 server.post('/api/messages', async (req, res) => {
